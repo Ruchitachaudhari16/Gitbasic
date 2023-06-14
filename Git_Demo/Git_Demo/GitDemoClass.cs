184b9ffa9cdd7617305af97fcfa96bc1d500c015
Ruchitachaudhari16 <ruchitachaudhari70@gmail.com>
2023-06-14 09:14:29 -0400

    [Ruchita] Add: Git demo

---
+++ Git_Demo/Git_Demo/GitDemoClass.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Git_Demo
+{
+    internal class GitDemoClass
+    {
+        public void Demo()
+        {
+            Console.WriteLine("Welcome");
+        }
+    }
+}
